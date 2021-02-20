177ce34f156dcbe986cdb2eb3ab8ca5108421ac2
vilmar <vilmarmoreira2010@gmail.com>
2021-02-20 16:04:52 -0300

    adiçaõ imagrns da api

--- js/app.js
+++ js/app.js
@@ -2,3 +2,31 @@
 
 const gato = document.getElementById('gato')
 const cachorro = document.getElementById('cachorro')
+
+const buttonGato = document.querySelectorAll(".subtitulo")[0]
+const buttonCachorro = document.querySelectorAll(".subtitulo")[1]
+console.log(buttonGato)
+console.log(buttonCachorro)
+const urlGato = 'https://aws.random.cat/meow'
+const urlCachorro = 'https://random.dog/woof.json'
+
+buttonGato.addEventListener('click',()=>{
+fetch(urlGato)
+    .then((res)=>res.json()    
+    )
+    .then(data=>{
+        gato.innerHTML = `<img src="${data.file}"     />`
+        console.log(data)
+})
+})
+
+
+buttonCachorro.addEventListener('click',()=>{
+    fetch(urlCachorro)
+    .then((res) => res.json())
+    .then(data => {
+        cachorro.innerHTML =`<img src="${data.url}"     />`
+        //cachorro.style.backgroundImage = "url('//')"
+        console.log(data.url)
+    })
+})
