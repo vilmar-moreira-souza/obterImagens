8ac5877891bc83a21b86f69ce575ec800b8ff0fd
vilmar <vilmarmoreira2010@gmail.com>
2021-02-22 17:40:45 -0300

    melhoria codigo

--- js/app.js
+++ js/app.js
@@ -2,15 +2,18 @@
 
 const gato = document.getElementById('gato')
 const cachorro = document.getElementById('cachorro')
-
+const fox = document.getElementById('fox')
 const buttonGato = document.querySelectorAll(".subtitulo")[0]
 const buttonCachorro = document.querySelectorAll(".subtitulo")[1]
+const buttonFox = document.querySelectorAll(".subtitulo")[2]
 console.log(buttonGato)
 console.log(buttonCachorro)
 const urlGato = 'https://aws.random.cat/meow'
 const urlCachorro = 'https://random.dog/woof.json'
+const urlFox = 'https://randomfox.ca/floof/'
 
-buttonGato.addEventListener('click',()=>{
+
+let fGato =()=>{
 fetch(urlGato)
     .then((res)=>res.json()    
     )
@@ -18,15 +21,32 @@ fetch(urlGato)
         gato.innerHTML = `<img src="${data.file}"     />`
         console.log(data)
 })
-})
+}
 
-
-buttonCachorro.addEventListener('click',()=>{
+let fCachorro =()=>{
     fetch(urlCachorro)
     .then((res) => res.json())
     .then(data => {
-        cachorro.innerHTML =`<img src="${data.url}"     />`
-        //cachorro.style.backgroundImage = "url('//')"
+
+        if (data.url.includes('.mp4')) {
+            fCachorro()
+        }else{
+        cachorro.innerHTML =`<img src="${data.url}"     />`}
+       
         console.log(data.url)
     })
-})
+}
+let fFox =()=>{
+    fetch(urlFox)
+        .then((res)=>res.json()    
+        )
+        .then(data=>{
+            fox.innerHTML = `<img src="${data.image}"     />`
+            console.log(data)
+    })
+    }
+
+buttonGato.addEventListener('click',fGato)
+buttonCachorro.addEventListener('click',fCachorro)
+buttonFox.addEventListener('click',fFox)
+
